2286bc0680e5ed571714c452021f1d853384dd2b
Bitex-ld <jtenesaca422@gmail.com>
2025-06-28 09:40:54 -0500

    añadido diccionario y tupla para mostrar jugadas y validar opciones

--- piedra_papel_tijera.py
+++ piedra_papel_tijera.py
@@ -1,8 +1,8 @@
-# Importamos la librería random para generar la jugada aleatoria del computador
+# Importamos la librería random para generar la jugada aleatoria del computador respectoa loas valores de la tupla
 import random
 
 
-#Diccionario que relaciona cada número con su jugada correspondiente
+# Diccionario que relaciona cada número con su jugada correspondiente
 opciones = {
     1: "🧱 Piedra",
     2: "📄 Papel",
@@ -20,27 +20,27 @@ while True:
     try:
         print("Elije una opcion: \n1 = 🧱 Piedra\n2 = 📄 Papel\n3 = ✂️ Tijera\n0 = 🚪 Salir del juego")
 
-        #Le pedrimos asl usuario que ingrese su jugada
+        # Le pedrimos asl usuario que ingrese su jugada
         jugada = int(input("Ingrese su jugada: "))
 
-        #Si escribe 0 sale del juego
+        # Si escribe 0 sale del juego
         if jugada == 0:
             print("👋 Gracias por jugar. ¡Hasta luego!")
             break
 
-        #Validamos la jugada del usuario que este entre el 1 y el 3
+        # Validamos la jugada del usuario que este entre el 1 y el 3
         if jugada not in opciones_validas:
             print("❌ Jugada inválida. Ingresa 1, 2 o 3.")
             continue
 
-        #la computadora genera una jugada aleatoria correspondiente a los valores de la tupla entre el 1 y el 3
+        # La computadora genera una jugada aleatoria correspondiente a los valores de la tupla entre el 1 y el 3
         jugada_pc = random.choice(opciones_validas)
 
-        #Mostramos la jugade del usuario y de la pc
+        # Mostramos la jugade del usuario y de la pc
         print(f"Tu elegiste la jugada: {opciones[jugada]}")
         print(f"La computadora eligio la jugada: {opciones[jugada_pc]}")
 
-        #Comparamos las jugadas
+        # Comparamos las jugadas
         if jugada == jugada_pc:
             print("🤝 Resultado: Empate")
         elif (jugada == 1 and jugada_pc == 3) or\
